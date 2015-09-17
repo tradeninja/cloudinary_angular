2a7871b8e89ce2dc72f554cef08df31b432f987b
kweestian <chrislhamelin@gmail.com>
2015-09-17 10:31:26 -0400

    commit de merde5

--- js/angular.cloudinary.js
+++ js/angular.cloudinary.js
@@ -86,7 +86,7 @@
       restrict : 'E',
       replace: true,
       transclude : true,
-      template: "<video controls>" + "<ng-transclude></ng-transclude>" + "</video>",
+      template: "<video controls>" + "<source ng-transclude>" + "</video>",
       scope: {},
       priority: 99,
       controller: Controller,
