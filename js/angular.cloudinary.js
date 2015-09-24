47407d4f3eab93f23fec679dadb631ccff65737a
Thomas Thiery <thomas.thiery@otolane.com>
2015-09-24 14:53:43 -0400

    test apply2

--- js/angular.cloudinary.js
+++ js/angular.cloudinary.js
@@ -104,30 +104,27 @@
                 attrs.$observe('publicId', function (value) {
                     if (!value) return;
                     publicId = value;
-                    loadImage();
+                    scope.$apply(loadImage());
                 });
 
                 attrs.$observe('type', function (value) {
                     if (!value) return;
                     attributes['type'] = value;
-                    loadImage();
+                    scope.$apply(loadImage());
                 });
 
                 attrs.$observe('options', function (value) {
                     if (!value) return;
                     options = JSON.parse(value);
-                    loadImage();
+                    scope.$apply(loadImage())
                 });
 
                 attrs.$observe('thumbnail', function (value) {
                     if (!value) return;
                     attributes['thumbnail'] = value;
-                    loadImage();
+                    scope.$apply(loadImage());
                 });
 
-                scope.$apply(function(){
-                    console.log('apply');
-                });
 
                 var loadImage = function () {
                     var media = "";
